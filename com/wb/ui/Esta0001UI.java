e7ccf2278ac15d869fba56646298a72ededacf07
leonngiam <leonngiam@hotmail.com>
2015-01-07 15:42:39 +0800

    Create Esta0001UI.java

---
+++ com/wb/ui/Esta0001UI.java
@@ -0,0 +1,217 @@
+package com.wb.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+
+public class Esta0001UI extends JPanel{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Esta0001UI(){
+		setLayout(new BorderLayout());
+		JPanel upperPane = new JPanel();
+		upperPane.setBackground(Color.WHITE);
+		upperPane.setPreferredSize(new Dimension(getWidth(), 250));
+		
+		JPanel masterPane = new JPanel(new GridBagLayout());
+		masterPane.setBackground(Color.WHITE);
+		masterPane.setPreferredSize(new Dimension(500, 250));		
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel estateLbl = new JLabel("Estate");
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        masterPane.add(estateLbl, c);
+
+        JLabel estateValueLbl = new JLabel("Estate ABC");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 0;
+        masterPane.add(estateValueLbl, c);
+
+        JLabel transporterLbl = new JLabel("Transporter");
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 1;
+        masterPane.add(transporterLbl, c);
+
+        JComboBox<String> transporterCb = new JComboBox<String>();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 1;
+        masterPane.add(transporterCb, c);
+
+        JLabel vehicleLbl = new JLabel("Vehicle");
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 2;
+        masterPane.add(vehicleLbl, c);
+
+        JComboBox<String> vehicleCb = new JComboBox<String>();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 2;
+        masterPane.add(vehicleCb, c);
+
+        JLabel driverLbl = new JLabel("Driver");
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 3;
+        masterPane.add(driverLbl, c);
+
+        JTextField driverTxt = new JTextField();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 3;
+        masterPane.add(driverTxt, c);
+
+        JLabel remarkLbl = new JLabel("Remarks");
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 4;
+        masterPane.add(remarkLbl, c);
+
+        JTextArea remarkTxt = new JTextArea(5,20);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 4;
+        masterPane.add(remarkTxt, c);
+		
+		upperPane.add(masterPane, BorderLayout.WEST);
+		
+		JPanel weighPane = new JPanel(new BorderLayout(5,5));
+		weighPane.setBackground(Color.WHITE);
+		
+		JPanel weighInPane = new JPanel(new GridBagLayout());
+		weighInPane.setPreferredSize(new Dimension(250, 100));
+		weighInPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Weigh In"));
+		weighInPane.setBackground(Color.WHITE);
+        GridBagConstraints weighInConstraint = new GridBagConstraints();
+        weighInConstraint.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel weighInDtLabel = new JLabel("Date");
+        weighInConstraint.weightx = 0.5;
+        weighInConstraint.fill = GridBagConstraints.HORIZONTAL;
+        weighInConstraint.gridx = 0;
+        weighInConstraint.gridy = 0;
+        weighInPane.add(weighInDtLabel, weighInConstraint);
+
+        JTextField weighInDtTxt = new JTextField();
+        weighInConstraint.fill = GridBagConstraints.HORIZONTAL;
+        weighInConstraint.weightx = 0.5;
+        weighInConstraint.gridx = 1;
+        weighInConstraint.gridy = 0;
+        weighInPane.add(weighInDtTxt, weighInConstraint);
+
+        JLabel weighInKgLabel = new JLabel("Weight (Kg)");
+        weighInConstraint.weightx = 0.5;
+        weighInConstraint.fill = GridBagConstraints.HORIZONTAL;
+        weighInConstraint.gridx = 0;
+        weighInConstraint.gridy = 1;
+        weighInPane.add(weighInKgLabel, weighInConstraint);
+
+        JTextField weighInKgTxt = new JTextField();
+        weighInConstraint.fill = GridBagConstraints.HORIZONTAL;
+        weighInConstraint.weightx = 0.5;
+        weighInConstraint.gridx = 1;
+        weighInConstraint.gridy = 1;
+        weighInPane.add(weighInKgTxt, weighInConstraint);
+		
+		weighPane.add(weighInPane, BorderLayout.PAGE_START);
+		
+		JPanel weighOutPane = new JPanel(new GridBagLayout());
+		weighOutPane.setPreferredSize(new Dimension(250, 100));
+		weighOutPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Weigh Out"));
+		weighOutPane.setBackground(Color.WHITE);
+        GridBagConstraints weighOutConstraint = new GridBagConstraints();
+        weighOutConstraint.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel weighOutDtLabel = new JLabel("Date");
+        weighOutConstraint.weightx = 0.5;
+        weighOutConstraint.fill = GridBagConstraints.HORIZONTAL;
+        weighOutConstraint.gridx = 0;
+        weighOutConstraint.gridy = 0;
+        weighOutPane.add(weighOutDtLabel, weighOutConstraint);
+
+        JTextField weighOutDtTxt = new JTextField();
+        weighOutConstraint.fill = GridBagConstraints.HORIZONTAL;
+        weighOutConstraint.weightx = 0.5;
+        weighOutConstraint.gridx = 1;
+        weighOutConstraint.gridy = 0;
+        weighOutPane.add(weighOutDtTxt, weighOutConstraint);
+
+        JLabel weighOutKgLabel = new JLabel("Weight (Kg)");
+        weighOutConstraint.weightx = 0.5;
+        weighOutConstraint.fill = GridBagConstraints.HORIZONTAL;
+        weighOutConstraint.gridx = 0;
+        weighOutConstraint.gridy = 1;
+        weighOutPane.add(weighOutKgLabel, weighOutConstraint);
+
+        JTextField weighOutKgTxt = new JTextField();
+        weighOutConstraint.fill = GridBagConstraints.HORIZONTAL;
+        weighOutConstraint.weightx = 0.5;
+        weighOutConstraint.gridx = 1;
+        weighOutConstraint.gridy = 1;
+        weighOutPane.add(weighOutKgTxt, weighOutConstraint);
+		
+		weighPane.add(weighOutPane, BorderLayout.CENTER);
+		upperPane.add(weighPane, BorderLayout.CENTER);
+		
+		add(upperPane, BorderLayout.PAGE_START);
+		
+		JPanel bottomPane = new JPanel(new BorderLayout());
+		bottomPane.setBackground(Color.WHITE);
+		bottomPane.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Details"));
+		
+		JTabbedPane bottomTab = new JTabbedPane();
+        Esta0001Form esta0001Form = new Esta0001Form();
+		JTable fieldList = new JTable(esta0001Form);
+		JScrollPane fieldPane = new JScrollPane(fieldList);
+		for(int i = 0; i < 10; i++){
+			esta0001Form.addRow(new String[7], i, i);
+		}
+		esta0001Form.addRow(new String[7], 1, 1);
+		esta0001Form.addRow(new String[7], 2, 2);
+		fieldList.setFillsViewportHeight(true);		
+		fieldList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		fieldList.setShowGrid(true);
+		
+		bottomTab.addTab("Fields", fieldPane);
+		bottomTab.addTab("Seal", new JPanel());
+		bottomPane.add(bottomTab, BorderLayout.CENTER);		
+		
+		add(bottomPane, BorderLayout.CENTER);
+		
+		
+	}	
+	
+}
