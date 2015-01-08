f841d6b501d6ad5cd1e7988d72b8184a60baed86
leonngiam <leonngiam@hotmail.com>
2015-01-08 11:21:57 +0800

    Update LoginUI.java

--- com/wb/ui/LoginUI.java
+++ com/wb/ui/LoginUI.java
@@ -1,14 +1,18 @@
 package com.wb.ui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -17,6 +21,7 @@ import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import com.wb.bo.ConnectionManager;
 import com.wb.bo.Criteria;
 import com.wb.bo.LoginInfo;
 import com.wb.bo.UserBO;
@@ -59,6 +64,90 @@ public class LoginUI extends JFrame{
 			final JTextField userText = new JTextField(20);
 			userText.setBounds(160, 100, 160, 25);
 			panel.add(userText);
+			userText.addKeyListener(new KeyListener() {
+				
+				@Override
+				public void keyTyped(KeyEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void keyReleased(KeyEvent arg0) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void keyPressed(KeyEvent e) {
+					// TODO Auto-generated method stub
+
+
+                    if(e.getKeyCode() == (KeyEvent.VK_F3) && e.isControlDown()){
+                    	final JDialog connDialog = new JDialog(LoginUI.this, "DB Connection");
+                    	connDialog.setSize(new Dimension(400,200));
+                    	connDialog.setBackground(Color.WHITE);
+                    	connDialog.setModal(true);
+                    	connDialog.setLayout(new GridLayout(5,2));
+                    	
+                        String driver = null;
+                        String url = null;
+                        String userName = null;
+                        String password= null;
+                    	
+                    	String[] connInfo = ConnectionManager.getConnectionFile();
+                    	
+                    	if(connInfo != null){
+                    		driver = connInfo[0];
+	                        url = connInfo[1];
+	                        userName = connInfo[2];
+	                        password= connInfo[3];               		
+                    	}
+                    	else{
+                        	
+                        	ResourceBundle rb = ResourceBundle.getBundle("jconfig");
+                        	
+                            driver = rb.getString("wb.driver");
+                            url = rb.getString("wb.url");
+                            userName = rb.getString("wb.username");
+                            password= rb.getString("wb.password");                    		
+                    	}
+                    	
+                    	connDialog.setBackground(Color.WHITE);
+                    	connDialog.add(new JLabel("Driver"));
+                    	final JTextField driverTxt = new JTextField(driver);
+                    	connDialog.add(driverTxt);
+                    	connDialog.add(new JLabel("URL"));
+                    	final JTextField urlTxt = new JTextField(url);
+                    	urlTxt.setColumns(10);
+                    	connDialog.add(urlTxt);
+                    	connDialog.add(new JLabel("Username"));
+                    	final JTextField userNameTxt = new JTextField(userName);
+                    	connDialog.add(userNameTxt);
+                    	connDialog.add(new JLabel("Password"));
+                    	final JPasswordField passwordTxt = new JPasswordField(password);
+                    	connDialog.add(passwordTxt);
+                    	connDialog.add(new JLabel(""));
+                    	JButton submitBtn = new JButton("Submit");
+                    	connDialog.add(submitBtn);
+                    	submitBtn.addActionListener(new ActionListener() {
+							
+							@Override
+							public void actionPerformed(ActionEvent e) {
+								// TODO Auto-generated method stub
+								ConnectionManager.createConnectionFile(driverTxt.getText(), urlTxt.getText(), userNameTxt.getText(), passwordTxt.getText());
+								connDialog.setVisible(false);
+								connDialog.dispose();
+							}
+						});
+                    	
+                    	connDialog.setResizable(false);
+                    	connDialog.pack();
+                    	connDialog.setVisible(true);
+                    	connDialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+                    }  					
+				}
+			});
 
 			JLabel passwordLabel = new JLabel("Password");
 			passwordLabel.setFont(new Font("Serif", Font.PLAIN, 14));
@@ -130,7 +219,7 @@ public class LoginUI extends JFrame{
 						new MainUI();									
 					}
 	            }
-	        });  
+	        });	
 			
 			JLabel copyrightLabel = new JLabel("Copyright 2014 | All rights reserved");
 			copyrightLabel.setFont(new Font("Serif", Font.PLAIN, 10));
