1a872ee2ca39c7af1c9adfc7827be5b2c50f3a6a
leonngiam <leonngiam@hotmail.com>
2015-01-27 09:08:59 +0800

    Update MainUI.java

--- com/wb/ui/MainUI.java
+++ com/wb/ui/MainUI.java
@@ -1,23 +1,25 @@
 package com.wb.ui;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.xml.parsers.DocumentBuilder;
@@ -38,101 +40,89 @@ public class MainUI extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
     private static final String MENU_XML = "menu.xml";
-    JTabbedPane editors = new JTabbedPane();
+
+    final JDesktopPane editors = new JDesktopPane();
 	
 	public MainUI(){
-		
 		try {
-			this.getContentPane().setBackground( Color.WHITE );
-			setTitle("Plantation Management System");
 			UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-			setLayout(new BorderLayout());
-			
-			JMenuBar menuBar = new JMenuBar();
-			JMenu fileMenu = new JMenu("File");
-			
-			JMenuItem cp = new JMenuItem("Change Password");
-			fileMenu.add(cp);
-			
-			cp.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent evt) {
-					// TODO Auto-generated method stub
-				    
-					new ChangePasswordDialog(MainUI.this);
-				}
-			});
-			fileMenu.add(cp);
-			JMenuItem logOutMenuItem = new JMenuItem("Log Out");
-			fileMenu.add(logOutMenuItem);        	
-			logOutMenuItem.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent evt) {
-					// TODO Auto-generated method stub
-					setVisible(false);
-					dispose();
-				}
-			});
-			menuBar.add(fileMenu);
-
-	        HashMap<String, List<String>> module = new HashMap<String, List<String>>();
-	        String[] viewSet = generateProgram(module);
-	        
-	        for (final String view : viewSet) {
-            	
-	        	List<String> programList = module.get(view);
-    
-	        	if(programList != null && programList.size() > 0){
-	        		JMenu component = new JMenu(view);
-	                for (Iterator<String> iterator = programList.iterator(); iterator.hasNext();) {
-	                	
-	                	String programId = iterator.next();
-	                	
-	                	JMenuItem menuItem = new JMenuItem(MenuProperties.getProgramName(programId));
-	                	menuItem.setActionCommand(programId);
-	                	
-	                	menuItem.addActionListener(new ActionListener() {
-							
-							@Override
-							public void actionPerformed(ActionEvent evt) {
-								// TODO Auto-generated method stub
-								createEditorInstance(evt.getActionCommand());
-							}
-						});
-	                	component.add(menuItem);
-	                }
-	                menuBar.add(component);
-	        	}	        	
-	        }
-			
-			add(menuBar, BorderLayout.PAGE_START);
-			
-			editors.setBackground(Color.WHITE);
-			add(editors, BorderLayout.CENTER);
-			
-			Toolkit tk = Toolkit.getDefaultToolkit();
-			int xSize = ((int) tk.getScreenSize().getWidth());
-			int ySize = ((int) tk.getScreenSize().getHeight());
-			
-			this.setSize(xSize, ySize);
-			this.setLocationRelativeTo(null);
-			setVisible(true);
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		} catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
+
+	    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	    screenSize.width -= 42;
+	    screenSize.height -= 42;
+	    setSize(screenSize);
+	    setLocation(0, 0);
+
+		JMenuBar menuBar = new JMenuBar();
+	    setJMenuBar(menuBar);
+		JMenu fileMenu = new JMenu("File");
+		
+		JMenuItem cp = new JMenuItem("Change Password");
+		fileMenu.add(cp);
+		
+		cp.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				// TODO Auto-generated method stub
+			    
+				new ChangePasswordDialog(MainUI.this);
+			}
+		});
+		fileMenu.add(cp);
+		JMenuItem logOutMenuItem = new JMenuItem("Log Out");
+		fileMenu.add(logOutMenuItem);        	
+		logOutMenuItem.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				// TODO Auto-generated method stub
+				setVisible(false);
+				dispose();
+			}
+		});
+		menuBar.add(fileMenu);
+
+        HashMap<String, List<String>> module = new HashMap<String, List<String>>();
+        String[] viewSet = generateProgram(module);
+        
+        for (final String view : viewSet) {
+        	
+        	List<String> programList = module.get(view);
+
+        	if(programList != null && programList.size() > 0){
+        		JMenu component = new JMenu(view);
+                for (Iterator<String> iterator = programList.iterator(); iterator.hasNext();) {
+                	
+                	String programId = iterator.next();
+                	
+                	JMenuItem menuItem = new JMenuItem(MenuProperties.getProgramName(programId));
+                	menuItem.setActionCommand(programId);
+                	
+                	menuItem.addActionListener(new ActionListener() {
+						
+						@Override
+						public void actionPerformed(ActionEvent evt) {
+							// TODO Auto-generated method stub
+							createEditorInstance(evt.getActionCommand());
+					    	  
+						}
+					});
+                	component.add(menuItem);
+                }
+                menuBar.add(component);
+        	}	        	
+        }
+	    
+	    setContentPane(editors);
+
+	    setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
     
     private String[] generateProgram(HashMap<String, List<String>> module){
@@ -189,11 +179,15 @@ public class MainUI extends JFrame{
     	
     }
 
-    public void createEditorInstance(String programId) {
+    public void createEditorInstance(String programId) {    	
+    	JInternalFrame pane = new JInternalFrame(MenuProperties.getProgramName(programId), true, true, true, true);
         String className = "com.wb.ui." + programId.substring(0, 1).toUpperCase() + programId.substring(1,8) + "UI";
-        JPanel view = new JPanel();
+        JPanel view = null;
         try {
-        	view =  (JPanel) Class.forName(className).newInstance();
+        	Constructor con = Class.forName(className).getConstructor(new Class[]{JDesktopPane.class, JInternalFrame.class});
+        	
+        	Object[] insta = new Object[]{editors, pane};
+        	view = (JPanel) con.newInstance(insta);
 	        
 		} catch (InstantiationException e) {
 			e.printStackTrace();
@@ -201,9 +195,28 @@ public class MainUI extends JFrame{
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
 		}
+        
+        if(view == null){
+          view = new JPanel();    
+          pane.setSize(200,200);
+          pane.setLocation(0, 0);
+          pane.setVisible(true);    	
+        }
 		
-        editors.addTab(MenuProperties.getProgramName(programId), view);
-        editors.setSelectedComponent(view);
+        pane.setContentPane(view);
+//        pane.setSize(800,600);
+//        pane.setLocation(0, 0);
+//        pane.setVisible(true);
+        editors.add(pane);
+        pane.moveToFront();
     }
 }
