9b77bea3a02595b402b5c119804af898adaeef40
leonngiam <leonngiam@hotmail.com>
2015-02-04 16:15:30 +0800

    Create TransporterBO.java

---
+++ com/wb/bo/TransporterBO.java
@@ -0,0 +1,23 @@
+package com.wb.bo;
+
+public class TransporterBO extends CommonBusinessObject{
+	
+	public static int FILTER_CODE = 0;
+	public static int FILTER_DESC = 1;
+	
+	private String code;
+	private String description;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
